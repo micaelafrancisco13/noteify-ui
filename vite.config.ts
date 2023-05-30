ee9529dc680eebd830ef04873684942b377a4125
Ela <micaela.francisco.s@bulsu.edu.ph>
2023-05-30 16:37:53 +0800

    added dark mode theme

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: `http://localhost:5173/`, // utilize port variable here
+  },
 })
