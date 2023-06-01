08b40fa6c78a0a4a6e39579fa9db12b5f8196ab8
Ela <micaela.francisco.s@bulsu.edu.ph>
2023-06-01 12:11:01 +0800

    add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
+import { useRef, useState } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./theme.ts";
 import NavBar from "./components/NavBar/NavBar.tsx";
-import { useRef, useState } from "react";
 import Main from "./components/Main/Main.tsx";
+import NotFound from "./components/NotFound.tsx";
 
 function App() {
   const [drawerToggle, setDrawerToggle] = useState(false);
   const drawerRef = useRef<HTMLDivElement>(null);
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <NavBar
-        drawerToggle={drawerToggle}
-        onDrawerToggle={(toggle) => setDrawerToggle(toggle)}
-        drawerRef={drawerRef}
-      />
-      {/*<Test />*/}
-      <Main drawerToggle={drawerToggle} drawerRef={drawerRef} />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <NavBar
+          drawerToggle={drawerToggle}
+          onDrawerToggle={(toggle) => setDrawerToggle(toggle)}
+          drawerRef={drawerRef}
+        />
+        <Routes>
+          <Route
+            path="/"
+            element={<Main drawerToggle={drawerToggle} drawerRef={drawerRef} />}
+          />
+          <Route path="/not-found" element={<NotFound />} />
+          <Route path="*" element={<Navigate to={`/not-found`} replace />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/NotFound.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+function NotFound() {
+  return (
+    <>
+      <Typography>NOT FOUND</Typography>
+    </>
+  );
+}
+
+export default NotFound;
