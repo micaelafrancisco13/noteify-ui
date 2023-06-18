8ce4fc536e444cf437b33ddbdfd1b9dec3f0931c
Ela <micaela.francisco.s@bulsu.edu.ph>
2023-06-18 23:08:19 +0800

    add display picture to account settings then make it responsive

--- src/components/Account/Account.tsx
+++ src/components/Account/Account.tsx
@@ -1,7 +1,8 @@
 import PersonalDetails from "./PersonalDetails.tsx";
 import EmailDetail from "./EmailDetail.tsx";
 import PasswordDetail from "./PasswordDetail.tsx";
-import { Stack } from "@mui/material";
+import { Grid, Stack } from "@mui/material";
+import DisplayPicture from "../DisplayPicture/DisplayPicture.tsx";
 
 interface Props {
   drawerToggle: boolean;
@@ -9,11 +10,39 @@ interface Props {
 
 function Account({ drawerToggle }: Props) {
   return (
-    <Stack spacing={8} sx={{ maxWidth: "500px" }}>
-      <PersonalDetails drawerToggle={drawerToggle} />
-      <EmailDetail drawerToggle={drawerToggle} />
-      <PasswordDetail drawerToggle={drawerToggle} />
-    </Stack>
+    <Grid
+      container
+      direction={{
+        xs: "column",
+        md: drawerToggle ? "column" : "row",
+        lg: "row",
+      }}
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Grid
+        item
+        xs={12}
+        md={6}
+        order={{ xs: 1, md: drawerToggle ? 1 : 2, lg: 2 }}
+        sx={{ width: "100%" }}
+      >
+        <DisplayPicture />
+      </Grid>
+      <Grid
+        item
+        xs={12}
+        md={6}
+        order={{ xs: 2, md: drawerToggle ? 2 : 1, lg: 1 }}
+        sx={{ width: "100%" }}
+      >
+        <Stack spacing={8}>
+          <PersonalDetails drawerToggle={drawerToggle} />
+          <EmailDetail drawerToggle={drawerToggle} />
+          <PasswordDetail drawerToggle={drawerToggle} />
+        </Stack>
+      </Grid>
+    </Grid>
   );
 }
 
