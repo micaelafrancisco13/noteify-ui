35b3374bc33e3cdb46ac152204640460b9f5d34f
Micaela Francisco <micaela.francisco.s@bulsu.edu.ph>
2023-08-26 21:05:45 +0800

    add hook for user registration

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,33 +1,13 @@
-import { useState } from "react";
-import { AxiosError } from "axios";
-import { SignUpFormData } from "../components/AuthForm/SignUpForm.tsx";
-import userService from "../services/user-service.ts";
-import useAuth from "./useAuth.ts";
+import userManager from "../services/user-manager.ts";
 
 function useUser() {
-    const [isCreatingAnAccount, setIsCreatingAnAccount] = useState(false);
-    const [createAnAccountError, setCreateAnAccountError] =
-        useState<AxiosError>();
-    const { signInUponRegistration } = useAuth();
-
-    const createAnAccount = (data: SignUpFormData) => {
-        const { confirmPassword, ...rest } = data;
-        setIsCreatingAnAccount(true);
-        userService
-            .create(rest)
-            .then((res) => {
-                setIsCreatingAnAccount(false);
-                const token = res.headers.authorization;
-                signInUponRegistration(token);
-                window.location.assign("/");
-            })
-            .catch((err) => {
-                setIsCreatingAnAccount(false);
-                setCreateAnAccountError(err);
-            });
+    const signUp = () => {
+        userManager.signinRedirect()
+            .then((response) =>  console.log(response))
+            .catch((exception) => console.log(exception));
     };
 
-    return { createAnAccount, isCreatingAnAccount, createAnAccountError };
+    return { createAnAccount: signUp };
 }
 
 export default useUser;
--- src/services/user-manager.ts
+++ src/services/user-manager.ts
@@ -2,14 +2,18 @@ import { UserManager } from "oidc-client-ts";
 
 export default new UserManager( {
     authority: "http://localhost:8000",
-    client_id: "noteify-react-client",
     metadata: {
-        authorization_endpoint:
-            "http://localhost:8000/realms/noteify-realm/protocol/openid-connect/auth",
-        token_endpoint:
-            "http://localhost:8000/realms/noteify-realm/protocol/openid-connect/token",
-        end_session_endpoint: "http://localhost:8000/realms/noteify-realm/protocol/openid-connect/logout"
+        issuer: "http://localhost:8000/realms/noteify-realm",
+        authorization_endpoint: "http://localhost:8000/realms/noteify-realm/protocol/openid-connect/auth",
+        registration_endpoint: "http://localhost:8000/realms/noteify-realm/login-actions/registration",
+        token_endpoint: "http://localhost:8000/realms/noteify-realm/protocol/openid-connect/token",
+        introspection_endpoint: "http://localhost:8000/realms/noteify-realm/protocol/openid-connect/token/introspect",
+        userinfo_endpoint: "http://localhost:8000/realms/noteify-realm/protocol/openid-connect/userinfo",
+        end_session_endpoint: "http://localhost:8000/realms/noteify-realm/protocol/openid-connect/logout",
+        jwks_uri: "http://localhost:8000/realms/noteify-realm/protocol/openid-connect/certs",
+        check_session_iframe: "http://localhost:8000/realms/noteify-realm/protocol/openid-connect/login-status-iframe.html",
     },
+    client_id: "noteify-react-client",
     redirect_uri: "http://localhost:5173/callback-sign-in",
     post_logout_redirect_uri: "http://localhost:5173/callback-sign-out",
 })
